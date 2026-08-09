0e9b7c02b30468c6f4f6b60c94bf8af2599a0b5c
Bhoomika P <pbhoomika1712@gmail.com>
2026-08-09 20:10:19 +0530

    Update matrix_cont.v

--- rtl/matrix_cont.v
+++ rtl/matrix_cont.v
@@ -12,16 +12,14 @@ output reg done
 
 );
 
-parameter IDLE    = 2'd0;
-parameter LOAD    = 2'd1;
+parameter IDLE = 2'd0;
+parameter LOAD = 2'd1;
 parameter COMPUTE = 2'd2;
-parameter FINISH  = 2'd3;
+parameter FINISH = 2'd3;
 
 reg [1:0] state,next_state;
 
-
 // State Register
-
 always @(posedge clk or negedge rst_n)
 begin
 
@@ -33,27 +31,20 @@ begin
 end
 
 // Next State Logic
-
 always @(*)
 begin
-
     next_state = state;
 
     case(state)
-
         IDLE:
             if(start)
                 next_state = LOAD;
-
         LOAD:
                 next_state = COMPUTE;
-
         COMPUTE:
                 next_state = FINISH;
-
         FINISH:
                 next_state = IDLE;
-
         default:
                 next_state = IDLE;
 
@@ -62,25 +53,20 @@ begin
 end
 
 // Output Logic
-
 always @(*)
 begin
 
-    load    = 0;
+    load = 0;
     compute = 0;
-    done    = 0;
+    done = 0;
 
     case(state)
-
         LOAD:
             load = 1;
-
         COMPUTE:
             compute = 1;
-
         FINISH:
             done = 1;
-
     endcase
 
 end
